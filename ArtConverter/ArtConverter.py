e320308f58d97d049a28e084d2f1e718ef20fd85
Penelope Morningstar <penelope.astrid.morningstar@gmail.com>
2025-01-02 17:02:52 -0600

    Prepare for adding a background to the game. Will need to switch assemblers to improve an issue with addressing.

--- ArtConverter/ArtConverter.py
+++ ArtConverter/ArtConverter.py
@@ -75,6 +75,84 @@ with open("ArtConverter/Tilemap.asm", "w") as f:
             f.write(line)
             line = "\n.byte "
             n=0
+            
+tilemap = Image.open("ArtConverter/background.png")
+tilemap = tilemap.convert("RGBA")
+tw,th = tilemap.size
+tilebytes = []
+for ty in range(32):
+    for tx in range(16):
+        for y in range(8):
+            for x in range(8):
+                r,g,b,a = tilemap.getpixel((tx*8+x,ty*8+y))
+                nr = math.floor((r/255.0)*32)
+                if nr == 32:
+                    nr = 31
+                ng = math.floor((g/255.0)*32)
+                if ng == 32:
+                    ng = 31
+                nb = math.floor((b/255.0)*32)
+                if nb == 32:
+                    nb = 31
+                ng = ng << 5
+                nb = nb << 10
+                half = nr+ng+nb
+                if half in colors:
+                    index = colors.index(half)
+                    tilebytes.append(index)
+                else:
+                    tilebytes.append(0)
+
+with open("ArtConverter/BGTiles.asm", "w") as f:
+    n = 0
+    line = ".byte "
+    for c in tilebytes:
+        n+=1
+        if(n!=1):
+            line +=","
+        line += hex(c)
+        if(n==8):
+            f.write(line)
+            line = "\n.byte "
+            n=0
+            
+tilemap = Image.open("ArtConverter/map1.png")
+tilemap = tilemap.convert("RGBA")
+tw,th = tilemap.size
+tilebytes = []
+for ty in range(32):
+    for tx in range(32):
+        r,g,b,a = tilemap.getpixel((tx,ty))
+        nr = math.floor((r/255.0)*32)
+        if nr == 32:
+            nr = 31
+        ng = math.floor((g/255.0)*32)
+        if ng == 32:
+            ng = 31
+        nb = math.floor((b/255.0)*32)
+        if nb == 32:
+            nb = 31
+        ng = ng << 5
+        nb = nb << 10
+        half = nr+ng+nb
+        if half in colors:
+            index = colors.index(half)
+            tilebytes.append(index)
+        else:
+            tilebytes.append(0)
+
+with open("ArtConverter/BGMap.asm", "w") as f:
+    n = 0
+    line = ".byte "
+    for c in tilebytes:
+        n+=1
+        if(n!=1):
+            line +=","
+        line += hex(c)
+        if(n==8):
+            f.write(line)
+            line = "\n.byte "
+            n=0
     
             
             
